c4e347a4d6d0b92ca251364ccfff291ffee12ed2
Sauleteh <sauljgz21@yahoo.es>
2023-11-24 17:24:06 +0100

    Últimas pruebas

--- SpeedControl/version.h
+++ SpeedControl/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define VERSION_BUILD 842
+#define VERSION_BUILD 843
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
